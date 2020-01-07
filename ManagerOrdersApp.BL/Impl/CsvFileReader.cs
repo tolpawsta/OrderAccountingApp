834d2576639581b45c13170a683e1135d9608c72
Pavel <tolpawsta@gmail.com>
2020-01-07 18:25:41 +0300

    Add CsvFileReader

---
+++ ManagerOrdersApp.BL/Impl/CsvFileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerOrdersApp.BL.Impl
+{
+    public class CsvFileReader
+    {
+    }
+}
